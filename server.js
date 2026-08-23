b99fd33fdf316cdc5b246717cbb54a26f7c42a31
krisz2026 <fuxi0517@gmail.com>
2026-08-23 12:22:07 +0200

    Initialize Express server with cart API

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+let bevetelek = 0
+app.post('/api/cart', (req,res)=>{
+ let cart = req.body.cart || []
+ let osszeg = 0
+ cart.forEach(i=>{ osszeg+= (i.price||0)*(i.qty||1) })
+ let dij = Math.round(osszeg*0.02)
+ bevetelek+=dij
+ res.json({ok:true, fee:dij, total:osszeg, bevetelek})
+})
+app.get('/', (req,res)=>{
+ res.send('SM Modul fut! Bevetel: '+bevetelek)
+})
+app.listen(3000, ()=>{console.log('Fut')})
